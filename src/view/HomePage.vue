98d6903fd5f12048807259e3a2fdb54e1142e8ed
nanbei <2659305925@qq.com>
2022-11-30 15:49:27 +0800

    初步获取通道信息

--- src/view/HomePage.vue
+++ src/view/HomePage.vue
@@ -13,7 +13,6 @@
                 <a class="user-phone">电话号码：{{ userinfo.phone }}</a><br>
                 <a class="user-email">邮箱地址：{{ userinfo.email }}</a><br>
                 <a class="user-school">学校：{{ userinfo.school }}</a><br>
-
               </div>
             </div>
           </el-card>
@@ -22,42 +21,20 @@
     </dev>
     <el-button type="primary" @click="listChannel">获取通道信息</el-button>
     <el-button type="primary" @click="lookXmlw">查看项目论文</el-button>
-    <el-table
-        :data="tableData"
-        stripe
-        style="width: 100%">
-      <el-table-column
-          label="id"
-          prop="id">
-      </el-table-column>
-      <el-table-column
-          label="name"
-          prop="name">
-      </el-table-column>
-      <el-table-column
-          label="type"
-          prop="type">
-      </el-table-column>
-      <el-table-column
-          label="creator"
-          prop="creator">
-      </el-table-column>
-      <el-table-column
-          label="creatorEmail"
-          prop="creatorEmail">
-      </el-table-column>
-      <el-table-column
-          label="score"
-          prop="score">
-      </el-table-column>
-      <el-table-column
-          label="due"
-          prop="due">
-      </el-table-column>
-      <el-table-column
-          align="right">
-      </el-table-column>
-    </el-table>
+    <el-tabs style="height: 200px;" stretch>
+      <el-tab-pane label="项目">
+        <el-table :data="tableData" stripe style="width: 100%">
+          <el-table-column label="id" prop="id"></el-table-column>
+          <el-table-column label="name" prop="name"></el-table-column>
+          <el-table-column label="type" prop="type"></el-table-column>
+          <el-table-column label="creator" prop="creator"></el-table-column>
+          <el-table-column label="creatorEmail" prop="creatorEmail"></el-table-column>
+          <el-table-column label="score" prop="score"></el-table-column>
+          <el-table-column label="due" prop="due"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="论文">论文</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -117,10 +94,6 @@ export default {
     listChannel() {
       axios.get((`mu/listChannel`)).then(res => {
         console.log(res.data);
-        this.$message({
-          message: res.data,
-          type: 'success'
-        })
         this.tableData = res.data;
         console.log(this.tableData);
       })
