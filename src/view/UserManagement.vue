09aaa4a3084fd94cffca065d6f217c0c14ecb3ac
nanbei <2659305925@qq.com>
2022-12-02 18:40:11 +0800

    用户管理和通道管理完成（分页和搜索版本）

--- src/view/UserManagement.vue
+++ src/view/UserManagement.vue
@@ -18,17 +18,37 @@
         </el-form-item>
       </el-form>
     </div>
-    <div style="position:absolute;left: 320px;width: 300px">
+    <div style="position:absolute;left: 320px;width: 500px">
       <el-button type="primary" @click="lookCode">查看未被使用的注册码</el-button>
-      <el-table :data="codeData" stripe style="width: 100%" @row-click="clickData">
+
+      <el-table :data="code_data" stripe
+                 style="width: 100%" @row-click="clickData">
         <el-table-column label="注册码" prop="code"></el-table-column>
-        <el-table-column label="姓名" prop="name"></el-table-column>
+        <el-table-column label="姓名" prop="name">
+        </el-table-column>
         <el-table-column label="权限" prop="isAdmin"></el-table-column>
+        <el-table-column><template slot="header" slot-scope="scope">
+          <el-input
+              v-model="codesearch"
+              size="mini"
+              placeholder="输入真实姓名搜索邀请码"/>
+        </template></el-table-column>
       </el-table>
+      <div class="pagination">
+        <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage"
+            :page-sizes="[5, 10, 20]"
+            :page-size="pagesize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total">
+        </el-pagination>
+      </div>
     </div>
-    <div style="position:absolute;left: 630px;width: 1000px;">
+    <div style="position:absolute;left: 830px;width: 1000px;">
       <el-button type="primary" @click="lookUsers">查看用户列表</el-button>
-      <el-table :data="userData" style="width: 1000px" :default-sort="{prop: 'date', order: 'ascending'}">
+      <el-table :data="user_data" style="width: 1000px" :default-sort="{prop: 'date', order: 'ascending'}">
         <el-table-column prop="date" label="用户头像">
         </el-table-column>
         <el-table-column prop="name" label="姓名">
@@ -40,12 +60,29 @@
         <el-table-column prop="id" label="注册顺序" sortable>
         </el-table-column>
         <el-table-column fixed="right" label="操作">
+          <template slot="header" slot-scope="scope">
+            <el-input
+                v-model="usersearch"
+                size="mini"
+                placeholder="输入用户名搜索用户"/>
+          </template>
           <template slot-scope="scope">
             <el-button @click.native.prevent="deleteRow(scope.$index, scope.row)" type="text" size="small">删除
             </el-button>
           </template>
         </el-table-column>
       </el-table>
+      <div class="pagination">
+        <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="currentPage"
+            :page-sizes="[5, 10, 20]"
+            :page-size="pagesize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="UserTotal">
+        </el-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -54,7 +91,7 @@
 import axios from "axios";
 
 export default {
-  name: "UserManagement",
+  //搜索数据
   data() {
     return {
       labelPosition: 'right',
@@ -64,10 +101,96 @@ export default {
         isAdmin: ''
       },
       codeData: [],
-      userData: []
+      userData: [],
+      codesearch:'',
+      usersearch:'',
+      currentPage:1,
+      pagesize:5,
+      total:0,
+      UserTotal:0
     };
   },
+  computed: {
+    code_data() {
+      let search = this.codesearch;
+      // 搜索功能
+      if (search){
+        let list =this.codeData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase())|| data.name.toLowerCase().includes(search.toLowerCase()));
+        // let fenYe = list.slice((this.currentPage-1)*this.pagesize,this.currentPage*this.pagesize);
+        return list
+      }
+      else {
+        let fenYe = this.codeData.slice((this.currentPage-1)*this.pagesize,this.currentPage*this.pagesize)
+        return fenYe
+      }
+    },
+    user_data(){
+      let search = this.usersearch;
+      // 搜索功能
+      if (search){
+        let list =this.userData.filter(data => !search || data.username.toLowerCase().includes(search.toLowerCase())|| data.username.toLowerCase().includes(search.toLowerCase()));
+        // let fenYe = list.slice((this.currentPage-1)*this.pagesize,this.currentPage*this.pagesize);
+        return list
+      }
+      else {
+        let fenYe = this.userData.slice((this.currentPage-1)*this.pagesize,this.currentPage*this.pagesize)
+        return fenYe
+      }
+    }
+  },
+  created: function (){
+  axios.get(`mu/showActivationCode`).then(res => {
+    if (res.status === 403) {
+      this.$message({
+        message: 'Unauthorized',
+        type: 'warning'
+      })
+    } else if (res.status === 200) {
+      this.codeData = res.data
+      this.total=this.codeData.length
+    }
+  })
+    axios.get(`mu/showAllUser`).then(res => {
+      if (res.status === 403) {
+        this.$message({
+          message: 'Unauthorized',
+          type: 'warning'
+        })
+      } else if (res.status === 200) {
+        this.userData = res.data
+        this.UserTotal=this.userData.length
+      }
+    })
+},
+  name: "UserManagement",
+
   methods: {
+    handleSizeChange(val) {
+      this.pagesize=val;
+      axios.get(`mu/showActivationCode`).then(res => {
+        if (res.status === 403) {
+          this.$message({
+            message: 'Unauthorized',
+            type: 'warning'
+          })
+        } else if (res.status === 200) {
+          this.codeData = res.data
+        }
+      })
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val;
+      axios.get(`mu/showActivationCode`).then(res => {
+        if (res.status === 403) {
+          this.$message({
+            message: 'Unauthorized',
+            type: 'warning'
+          })
+        } else if (res.status === 200) {
+          this.codeData = res.data
+        }
+      })
+    },
     deleteRow(index, rows) {
       axios.delete(`mu/deleteUser?email=${rows.email}`).then(res => {
         if (res.status === 403) {
@@ -93,6 +216,7 @@ export default {
               })
             } else if (res.status === 200) {
               this.userData = res.data
+              this.UserTotal=this.userData.length
             }
           })
         }
@@ -166,7 +290,8 @@ export default {
           }
         })
       }
-    }
+    },
+
   }
 }
 </script>
