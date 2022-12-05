da2b7fc5f24eb026d48a681ac722f6167598e3a4
yumingrui20 <1730808730@qq.com>
2022-12-06 01:53:11 +0800

    项目详情页已完成(接收id和email)

--- src/view/ProjectDetail.vue
+++ src/view/ProjectDetail.vue
@@ -1,22 +1,43 @@
 <template>
-  <el-descriptions title="项目信息">
-    <el-descriptions-item label="项目名称">{{project.name}}</el-descriptions-item>
-    <el-descriptions-item label="负责人">{{ project.maintainer }}</el-descriptions-item>
-    <el-descriptions-item label="项目简介">{{ project.description }}</el-descriptions-item>
-    <el-descriptions-item label="项目类别">
-      <el-tag size="small">{{ project.channelId }}</el-tag>
-    </el-descriptions-item>
-    <el-descriptions-item label="负责单位">{{ project.company }}</el-descriptions-item>
-    <el-descriptions-item label="开始年份">{{project.setYear}}</el-descriptions-item>
-    <el-descriptions-item label="金额">{{project.money}}</el-descriptions-item>
-    <el-descriptions-item label="上传时间">{{project.setTime}}</el-descriptions-item>
+  <div>
+    <el-descriptions title="项目信息">
+      <el-descriptions-item label="项目名称">{{project.name}}</el-descriptions-item>
+      <el-descriptions-item label="负责人">{{ project.maintainer }}</el-descriptions-item>
+      <el-descriptions-item label="项目简介">{{ project.description }}</el-descriptions-item>
+      <el-descriptions-item label="项目类别">
+        <el-tag size="small">{{ showChannel }}</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="负责单位">{{ project.company }}</el-descriptions-item>
+      <el-descriptions-item label="开始年份">{{project.startYear}}</el-descriptions-item>
+      <el-descriptions-item label="金额">{{project.money}}</el-descriptions-item>
+      <el-descriptions-item label="上传时间">{{project.setTime}}</el-descriptions-item>
+      <el-descriptions-item label="项目状态">
+        <el-tag size="small">{{ project.status}}</el-tag>
+      </el-descriptions-item>
+      <el-descriptions-item label="下载">
+        <el-button @click="downloadProfile">点击下载项目文件</el-button>
+        <el-button @click="downloadPic">点击下载图片凭证</el-button>
+      </el-descriptions-item>
+    </el-descriptions>
 
-    <el-form-item>
-      <el-button type="primary" @click="onSubmit">上传项目</el-button>
-      <el-button>取消</el-button>
-    </el-form-item>
-  </el-descriptions>
+    <el-button @click="showEditDialog" v-if="userinfo.isAdmin == 0">审核</el-button>
 
+    <el-card class="box-card" style="position: absolute;left: 400px" v-if="editDialogVisible">
+      <el-dialog
+          title="请问您的审核结果是？"
+          :visible.sync="editDialogVisible"
+          width="50%" @close="editDialogClosed">
+        <el-radio v-model="editStatus" label="Accept">审核通过</el-radio>
+        <el-radio v-model="editStatus" label="Reject">审核驳回</el-radio>
+        <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false; editStatus ='Waiting' ">取 消</el-button>
+        <el-button type="primary" @click="changeStatus">确 定</el-button>
+      </span>
+      </el-dialog>
+    </el-card>
+  </div>
+
+<!--  <el-button @click="updateProject" v-else>修改论文</el-button>-->
 </template>
 
 <script>
@@ -28,6 +49,8 @@ export default {
   data(){
     return{
       showChannel:"",
+      editDialogVisible:false,
+      editStatus:"",
       userinfo: {
         "id":"",
         "username": "",
@@ -39,8 +62,8 @@ export default {
         "code": "",
         "name": ""
       },
-      imageUrl: '',
       project: {
+        id:'',
         name: '',
         uploaderEmail: '',
         maintainer: '',
@@ -49,21 +72,22 @@ export default {
         company: '',
         money: '',
         setTime: '',
-        setYear: ''
+        status: '',
+        startYear: ''
       },
     }
   },
   created() {
     let that = this;
-    // this.$data.project.channelId = this.$route.query.channelId
-    this.$data.project.channelId =5
-    console.log(this.$route.query.channelId)
-    // this.$data.userinfo.email = this.$route.query.email
-    this.$data.userinfo.email ="10204500101@stu.ecnu.edu.cn"
-    console.log(this.$route.query.email)
-    getChannelAndUser(this.$data.project.channelId, this.$data.userinfo.email);
+    this.$data.project.id = this.$route.query.id
+    // this.$data.project.Id = 5
+    console.log(this.$data.project.id)
+    this.$data.userinfo.email = this.$route.query.email
+    // this.$data.userinfo.email ="1730808730@qq.com"
+    console.log(this.$data.userinfo.email)
+    getUSerAndProject(this.$data.userinfo.email, this.$data.project.Id);
 
-    function getChannelAndUser(channel, email) {
+    function getUSerAndProject(email, id) {
       axios.get(`mu/getUsernames/email=${email}`, {
         email: email,
       }).then(res => {
@@ -80,19 +104,57 @@ export default {
         that.$data.userinfo.code = res.data.code;
         that.$data.project.uploaderEmail = res.data.email;
       });
-      axios.get(`mu/getChannelById?id=${channel}`, {
-        id: channel
+      axios.get(`mu/project/queryById?id=${id}`, {
+        id: id
       }).then(res => {
         console.log(res.data);
-        console.log("打印通道信息")
-        that.$data.project.channelId = res.data.id;
-        that.$data.showChannel = res.data.name;
-      })
+        console.log("项目信息")
+        that.$data.project.id = res.data.id;
+        that.$data.project.channelId=res.data.channelId;
+        that.$data.project.name = res.data.name;
+        that.$data.project.maintainer = res.data.maintainer;
+        that.$data.project.description = res.data.description;
+        that.$data.project.company = res.data.company;
+        that.$data.project.money = res.data.money;
+        that.$data.project.setTime = res.data.setTime;
+        that.$data.project.status = res.data.status;
+        that.$data.project.startYear = res.data.startTime;
+        axios.get(`mu/getChannelById?id=${that.$data.project.channelId}`, {
+          id: that.$data.project.channelId
+        }).then(res => {
+          console.log(res.data);
+          console.log("打印通道信息")
+          that.$data.showChannel = res.data.name;
+        })
+      });
     }
   },
   methods: {
-    onSubmit() {
-      console.log('submit!');
+    showEditDialog(){
+      this.$data.editDialogVisible = true;
+    },
+    editDialogClosed(){
+      this.$refs.editFormRef.resetFields()
+    },
+    changeStatus(){
+      let that = this;
+      axios.post(`mu/project/changeStatus?id=${that.$data.project.id}&status=${that.$data.editStatus}`, {
+        id: that.$data.project.Id,
+        status: that.$data.editStatus
+      }).then(res => {
+        console.log("修改状态成功")
+        console.log(res.data)
+      });
+      this.editDialogVisible = false
+      this.project.status = this.editStatus
+    },
+    downloadProfile(){
+      let that = this;
+      window.open(`mu/project/download/${that.$data.project.id}/zip`,"_blank")
+    },
+    downloadPic(){
+      let that = this;
+      window.open(`mu/project/download/${that.$data.project.id}/fig`,"_blank")
     }
   }
 
